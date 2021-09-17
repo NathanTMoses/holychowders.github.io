92c8767cfe60eb90077e7b47bfd4b8663a100c8b
holychowders <theholychowders@gmail.com>
2021-09-16 22:39:57 -0500

    Re-structure Interests page to show topics instead of media formats first
    For example, do:
    Interests -> Computer Science -> Blogs
    
    Instead of:
    Interests -> Blogs -> Computer Science

---
+++ pages/interests/_assets/include_header.js
@@ -0,0 +1,18 @@
+document.write(`
+<header id="main-header">
+    <h1>
+        <a href="../../main/main.html">
+            HolyChowders
+        </a>
+    </h1>
+
+    <nav>
+    <hr />
+    <a id="main-header-navbar-about" href="../../about/about.html">About</a>
+    <a id="main-header-navbar-posts" href="../../posts/posts.html">Posts</a>
+    <a id="main-header-navbar-activities" href="../../activities/activities.html">Activities</a>
+    <a id="main-header-navbar-interests" href="../../interests/interests.html">Interests</a>
+    <hr />
+    </nav>
+</header>
+`);
